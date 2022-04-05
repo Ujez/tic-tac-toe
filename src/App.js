777476b0c8d8b4b55fe929d14a883e3fc0b26ba0
ujez <ujezujez@gmail.com>
2022-04-05 12:21:39 +0100

    lifting up state from the board to the Game component

--- src/App.js
+++ src/App.js
@@ -11,11 +11,9 @@ function Square(props) {
 }
 
 class Board extends React.Component {
-
-
   handleClick(i) {
     const squares = this.state.squares.slice();
-    
+
     if (calculateWinner(squares) || squares[i]) {
       return;
     }
@@ -26,8 +24,8 @@ class Board extends React.Component {
   renderSquare(i) {
     return (
       <Square
-        value={this.state.squares[i]}
-        onClick={() => this.handleClick(i)}
+        value={this.props.squares[i]}
+        onClick={() => this.props.onClick(i)}
       />
     );
   }
@@ -68,7 +66,9 @@ class Game extends React.Component {
   constructor(props) {
     super(props);
     this.state = {
-      squares: Array(9).fill(null),
+      history: [{
+        squares: Array(9).fill(null),
+      }],
       xIsNext: true,
     };
   }
